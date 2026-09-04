55a217d7f2e76f8c6fd0b3fee4180d3c474f6d8e
Thomas Richter <th.richter@uksh.de>
2026-09-04 15:47:39 +0200

    use DIMP_DUP_YAML_PATH with dimp_dup_project.yaml

--- data-node/fhir-pseudonymizer/run.sh
+++ data-node/fhir-pseudonymizer/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export COMPOSE_PROJECT=${DATA_PORTAL_COMPOSE_PROJECT:-dataportal}
 #docker compose -p "$COMPOSE_PROJECT" -f docker-compose.vfps.yml up -d
+# ln -vs ~/projects/KePop/kepop_2026-08/dimp_dup_base.yaml dimp_dup_project.yaml
+# ln -vs ~/projects/KePop/kepop_2026-08/kepop.env project.env
+export DIMP_DUP_YAML_PATH="dimp_dup_project.yaml"
 docker compose --env-file .env --env-file project.env -p "$COMPOSE_PROJECT" -f docker-compose.yml up -d
-
