57a01b00a509a1115beffc04995b156b2140836f
Thomas Richter <th.richter@ksh.de>
2026-09-04 15:54:48 +0200

    another project var? use local path

--- data-node/fhir-pseudonymizer/run.sh
+++ data-node/fhir-pseudonymizer/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export COMPOSE_PROJECT=${DATA_PORTAL_COMPOSE_PROJECT:-dataportal}
+export DATA_PORTAL_COMPOSE_PROJECT=${DATA_PORTAL_COMPOSE_PROJECT:-dataportal}
 #docker compose -p "$COMPOSE_PROJECT" -f docker-compose.vfps.yml up -d
 # ln -vs ~/projects/KePop/kepop_2026-08/dimp_dup_base.yaml dimp_dup_project.yaml
 # ln -vs ~/projects/KePop/kepop_2026-08/kepop.env project.env
-export DIMP_DUP_YAML_PATH="dimp_dup_project.yaml"
+export DIMP_DUP_YAML_PATH="./dimp_dup_project.yaml"
 docker compose --env-file .env --env-file project.env -p "$COMPOSE_PROJECT" -f docker-compose.yml up -d
